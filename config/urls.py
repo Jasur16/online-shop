a3945ca1f7d4599836852fc982affe645eff82ef
Jasur <jasur@example.com>
2022-07-13 15:42:43 +0500

    ...

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pages.urls'))
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.SATATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
